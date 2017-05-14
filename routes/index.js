a7960eecb4f18cee3b0c1e2c07e4a348a1f709c6
Ali Fathieh <ali.fathieh@heyyou.com.au>
2017-05-15 09:43:35 +1000

    test again

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express3' });
+  res.render('index', { title: 'Express3x' });
 });
 
 module.exports = router;
+
